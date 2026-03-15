b8709c54bf5a9b7d5f44573cbe94a44e8fefc8f0
horiz <horiz@Horizon.com>
2026-03-15 11:07:45 -0400

    Show as cylinder tank. Group tsnks.

--- tank_calculations/main.cpp
+++ tank_calculations/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -9,21 +10,21 @@ float Volume (float diameter, float height) {
     return PI * r * r * height;
 }
 
-struct Tank {
+struct CylinderTank {
     int capacity;
     float diameter;
     float height;
     float volume;
 
-    Tank(int c, float d, float h) {
+    CylinderTank(int c, float d, float h) {
         capacity = c;
         diameter = d;
         height = h;
         volume = Volume(d, h);
     }
 
-    void print() const {
-        cout << "Tank Details:" << endl;
+    void printCylinderTankDetails() const {
+        cout << "Cylinder Tank Details:" << endl;
         cout << " Capacity: " << capacity << " gallons" << endl;
         cout << " Diameter: " << diameter << " ft" << endl;
         cout << " Height: " << height << " ft" << endl;
@@ -34,19 +35,35 @@ struct Tank {
 
 int main()
 {
-    cout << "Volumes" << endl;
+    // t_2k
+    float t_2kCap = 2000;
+    vector<float> t_2kDem = {8, 5.3};
+    CylinderTank t_2k{t_2kCap, t_2kDem[0], t_2kDem[1]};
+    t_2k.printCylinderTankDetails();
 
-    Tank t_2k{2000,   8,  5.3};
-    Tank t_5k{5000,   10, 8.5};
-    Tank t_10k{10000, 12, 11.8};
-    Tank t_15k{15000, 14, 13.0};
-    Tank t_20k{20000, 16, 13.3};
+    // t_5k
+    float t_5kCap = 5000;
+    vector<float> t_5kDem = {10, 8.5};
+    CylinderTank t_5k{t_5kCap, t_5kDem[0], t_5kDem[1]};
+    t_5k.printCylinderTankDetails();
 
-    t_2k.print();
-    t_5k.print();
-    t_10k.print();
-    t_15k.print();
-    t_20k.print();
+    // t_10k
+    float t_10kCap = 10000;
+    vector<float> t_10kDem = {12, 11.8};
+    CylinderTank t_10k{t_10kCap, t_10kDem[0], t_10kDem[1]};
+    t_10k.printCylinderTankDetails();
+
+    // t_15k
+    float t_15kCap = 15000;
+    vector<float> t_15kDem = {14, 13.0};
+    CylinderTank t_15k{t_15kCap, t_15kDem[0], t_15kDem[1]};
+    t_15k.printCylinderTankDetails();
+
+    // t_20k
+    float t_20kCap = 20000;
+    vector<float> t_20kDem = {16, 13.3};
+    CylinderTank t_20k{t_20kCap, t_20kDem[0], t_20kDem[1]};
+    t_20k.printCylinderTankDetails();
 
     return 0;
 }
